7e2af8c70868108fff752a6155b697017677551a
hsu-git <hsu080206@gmail.com>
2025-01-24 19:04:23 +0900

    re

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ async function makeIssue() {
     const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
         method: 'POST',
         headers: {
-            Authorization: `Bearer $(token)`,
+            Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-        title: "행운의 숫자",   // 변경
-        body: `$(Math.floor(Math.random()*100)+1)`, // 변경
+            title: "행운의 숫자",    // 변경
+            body: `${Math.floor(Math.random() * 100) + 1}`, // 변경
         })
     });
-    if(responer.ok){
+    if (response.ok) {
         console.log("성공");
     } else {
         console.log("실패");
