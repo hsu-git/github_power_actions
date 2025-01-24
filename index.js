82e9baf092eaf2ff5861aec9cad5f0c6311df8cf
hsu-git <hsu080206@gmail.com>
2025-01-24 18:09:26 +0900

    ㅇ

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 async function makeIssue() {
-    const token = process.env.GITHUB_TOKEN;
+    const token = process.env.GH_TOKEN; // 주의! 일치해야함 (env와 일치해야함)
+    // 요고 1
     const OWNER = "hsu-git"; // github 계정 이름
+    // 요고 2
     const REPO = "github_power_actions"; //현재 레포지터리 이름
     const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
         method: 'POST',
